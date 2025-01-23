144f4def92be2e84cb44ef2fffce502eb28e04d0
Denis YepBro <denis@yepbro.ru>
2025-01-23 21:40:47 +0300

    Fix phpstan errors

--- src/Exceptions/EloquentValidatorException.php
+++ src/Exceptions/EloquentValidatorException.php
@@ -5,6 +5,10 @@ namespace YepBro\EloquentValidator\Exceptions;
 
 use Exception;
 
+/**
+ * @property int $code
+ * @property string $message
+ */
 class EloquentValidatorException extends Exception
 {
     //
