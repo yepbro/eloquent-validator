d5216afc42dd49cf654c499648ce9737c6e4222e
Denis YepBro <denis@yepbro.ru>
2025-01-26 10:06:35 +0300

    Casting test stubs

---
+++ tests/Feature/Casts/AsEncryptedCollectionCastTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace YepBro\EloquentValidator\Tests\Feature\Casts;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+
+class AsEncryptedCollectionCastTest extends FeatureTestCase
+{
+    use DatabaseMigrations;
+
+    protected bool $fakeProduct = false;
+}
---
+++ tests/Feature/Casts/RealCastTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace YepBro\EloquentValidator\Tests\Feature\Casts;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+
+class RealCastTest extends FeatureTestCase
+{
+    use DatabaseMigrations;
+
+    protected bool $fakeProduct = false;
+}
