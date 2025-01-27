6eac3eafd22394a9b4bc6fa78d487b2193d4d938
Denis YepBro <denis@yepbro.ru>
2025-01-27 21:28:09 +0300

    php-cs-fixer configuration

---
+++ .php_cs.php
@@ -0,0 +1,17 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/src');
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'new_with_parentheses' => [
+            'anonymous_class' => false,
+            'named_class' => false,
+        ],
+        'trailing_comma_in_multiline' => [
+            'elements' => ['arguments', 'array_destructuring', 'arrays', 'match', 'parameters'],
+        ],
+    ])
+    ->setFinder($finder);
