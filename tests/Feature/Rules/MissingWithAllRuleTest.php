5a36e3e6ec952006d54244bcf847d07b8349b137
Denis YepBro <denis@yepbro.ru>
2025-01-29 22:55:07 +0300

    Dumps for rule feature tests

---
+++ tests/Feature/Rules/MissingWithAllRuleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace YepBro\EloquentValidator\Tests\Feature\Rules;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+
+class MissingWithAllRuleTest extends FeatureTestCase
+{
+    use DatabaseMigrations;
+
+    public function test_ok()
+    {
+        $this->markTestSkipped();
+    }
+}
