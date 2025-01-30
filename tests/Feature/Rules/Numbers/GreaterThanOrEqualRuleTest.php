84ddcf3382f677aa57f85b39702d1d71de3f1298
Denis YepBro <denis@yepbro.ru>
2025-01-30 13:05:33 +0300

    Greater Than Or Equal rule feature tests

--- tests/Feature/Rules/Numbers/GreaterThanOrEqualRuleTest.php
+++ tests/Feature/Rules/Numbers/GreaterThanOrEqualRuleTest.php
@@ -4,15 +4,38 @@ namespace YepBro\EloquentValidator\Tests\Feature\Rules\Numbers;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
 #[Group('Rules')]
-class GreaterThanOrEqualRuleTest extends FeatureTestCase
+#[Group('NumberRules')]
+class GreaterThanOrEqualRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    public function test_validation_of_incorrect_data_with_a_stringable_rule()
     {
-        $this->markTestSkipped();
+        $this->testException('gte:other', [
+            'other' => 5,
+            'field' => 4,
+        ], 'gte.numeric');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([6, 5])]
+    #[TestWith([5, 5])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(int $field, int $other)
+    {
+        $this->testSuccess('gte:other', [
+            'other' => $other,
+            'field' => $field,
+        ]);
     }
 }
