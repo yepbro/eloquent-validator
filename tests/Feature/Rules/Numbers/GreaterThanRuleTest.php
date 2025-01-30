252b8cba92bf06054400b1bef4d008706175011b
Denis YepBro <denis@yepbro.ru>
2025-01-30 13:01:53 +0300

    Greater Than rule feature tests

--- tests/Feature/Rules/Numbers/GreaterThanRuleTest.php
+++ tests/Feature/Rules/Numbers/GreaterThanRuleTest.php
@@ -4,15 +4,35 @@ namespace YepBro\EloquentValidator\Tests\Feature\Rules\Numbers;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use PHPUnit\Framework\Attributes\Group;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
 #[Group('Rules')]
-class GreaterThanRuleTest extends FeatureTestCase
+#[Group('NumberRules')]
+class GreaterThanRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    public function test_validation_of_incorrect_data_with_a_stringable_rule()
     {
-        $this->markTestSkipped();
+        $this->testException('gt:other', [
+            'other' => 5,
+            'field' => 5,
+        ], 'gt.numeric');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    public function test_validation_of_correct_data_with_a_stringable_rule()
+    {
+        $this->testSuccess('gt:other', [
+            'other' => 5,
+            'field' => 6,
+        ]);
     }
 }
