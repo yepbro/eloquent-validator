a77f1b31d9092593c991b9cc76fef512b9f3236a
Denis YepBro <denis@yepbro.ru>
2025-02-01 17:20:52 +0300

    Array min rule feature tests

--- tests/Feature/Rules/Arrays/MinRuleTest.php
+++ tests/Feature/Rules/Arrays/MinRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Arrays;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class MinRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('ArrayRules')]
+class MinRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([[1]])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value): void
     {
-        $this->markTestSkipped();
+        $this->testException('array|min:2', ['field' => $value], 'min.array');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([[1, 2, 3]])]
+    #[TestWith([[1, 2]])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value): void
+    {
+        $this->testSuccess('array|min:2', ['field' => $value]);
     }
 }
