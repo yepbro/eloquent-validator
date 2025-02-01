7184783922406a6d3e32a44a88b8b88cd042811d
Denis YepBro <denis@yepbro.ru>
2025-02-01 18:02:14 +0300

    Skip file rules tests

--- tests/Feature/Rules/Files/FileRuleTest.php
+++ tests/Feature/Rules/Files/FileRuleTest.php
@@ -3,14 +3,17 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Files;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
 
+#[Group('Rules')]
+#[Group('FileRules')]
 class FileRuleTest extends FeatureTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    public function test_skip()
     {
-        $this->markTestSkipped();
+        $this->markTestSkipped('The validation rule is not applicable to models.');
     }
 }
