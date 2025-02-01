daaba61ae8ae09dc73b6f21c870daed0a178bac2
Denis YepBro <denis@yepbro.ru>
2025-02-01 17:22:03 +0300

    Array size rule feature tests

--- tests/Feature/Rules/Arrays/SizeRuleTest.php
+++ tests/Feature/Rules/Arrays/SizeRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Arrays;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class SizeRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('ArrayRules')]
+class SizeRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([[1]])]
+    #[TestWith([[1, 2, 3]])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value): void
     {
-        $this->markTestSkipped();
+        $this->testException('array|size:2', ['field' => $value], 'size.array');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([[1, 2]])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value): void
+    {
+        $this->testSuccess('array|size:2', ['field' => $value]);
     }
 }
