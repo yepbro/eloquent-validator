f7353e35ec4c8bea9f11c43f30407f13468f35df
Denis YepBro <denis@yepbro.ru>
2025-02-01 16:48:27 +0300

    Array distinct rule feature tests

--- tests/Feature/Rules/Arrays/DistinctRuleTest.php
+++ tests/Feature/Rules/Arrays/DistinctRuleTest.php
@@ -3,14 +3,22 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Arrays;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use PHPUnit\Framework\Attributes\Group;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class DistinctRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('ArrayRules')]
+class DistinctRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(): void
     {
-        $this->markTestSkipped();
+        $this->markTestSkipped('The validation rule is not applicable to models.');
+    }
+
+    public function test_validation_of_correct_data_with_a_stringable_rule(): void
+    {
+        $this->markTestSkipped('The validation rule is not applicable to models.');
     }
 }
