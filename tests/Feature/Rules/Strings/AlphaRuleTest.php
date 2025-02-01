af5010a68c59ff7bb1ccc867ad4b71f7ea3c6e76
Denis YepBro <denis@yepbro.ru>
2025-02-01 20:18:41 +0300

    Alpha rule feature tests

--- tests/Feature/Rules/Strings/AlphaRuleTest.php
+++ tests/Feature/Rules/Strings/AlphaRuleTest.php
@@ -3,14 +3,39 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class AlphaRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class AlphaRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['aaa^gg'])]
+    #[TestWith(['aaa_gg'])]
+    #[TestWith(['aaa-gg'])]
+    #[TestWith(['aaa1gg'])]
+    #[TestWith(['аврора^gg', ':ascii'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value, string $param = '')
     {
-        $this->markTestSkipped();
+        $this->testException("alpha$param", $value, 'alpha');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['aaa'])]
+    #[TestWith(['проверка'])]
+    #[TestWith(['test', ':ascii'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value, string $param = '')
+    {
+        $this->testSuccess("alpha$param", $value);
     }
 }
