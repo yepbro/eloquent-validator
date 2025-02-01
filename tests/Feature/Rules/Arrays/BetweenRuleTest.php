830df84911e499430ed6b38d87c39f721456139f
Denis YepBro <denis@yepbro.ru>
2025-02-01 16:15:55 +0300

    Array between rule feature tests

--- tests/Feature/Rules/Arrays/BetweenRuleTest.php
+++ tests/Feature/Rules/Arrays/BetweenRuleTest.php
@@ -3,14 +3,35 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Arrays;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class BetweenRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('ArrayRules')]
+class BetweenRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([[1]])]
+    #[TestWith([[1, 2, 3, 4, 5]])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value): void
     {
-        $this->markTestSkipped();
+        $this->testException('array|between:2,4', ['field' => $value], 'between.array');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([[1, 2]])]
+    #[TestWith([[1, 2, 3]])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value): void
+    {
+        $this->testSuccess('array|between:2,4', ['field' => $value]);
     }
 }
