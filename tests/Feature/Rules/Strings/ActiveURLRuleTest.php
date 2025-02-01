878afc3dc7785bd890a3aef0840b3597b3f69ba0
Denis YepBro <denis@yepbro.ru>
2025-02-01 20:07:10 +0300

    Active URL rule feature tests

--- tests/Feature/Rules/Strings/ActiveURLRuleTest.php
+++ tests/Feature/Rules/Strings/ActiveURLRuleTest.php
@@ -3,14 +3,31 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Str;
+use PHPUnit\Framework\Attributes\Group;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class ActiveURLRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class ActiveURLRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    public function test_validation_of_incorrect_data_with_a_stringable_rule()
     {
-        $this->markTestSkipped();
+        $this->testException('active_url', 'https://'.Str::random(32).'.ru/test');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    public function test_validation_of_correct_data_with_a_stringable_rule()
+    {
+        $this->testSuccess('active_url', 'https://ya.ru/test');
     }
 }
