ba3cc24e5add3114253c7dd74cda321f0be03a5c
Denis YepBro <denis@yepbro.ru>
2025-02-01 16:56:09 +0300

    Array "in array" rule feature tests

--- tests/Feature/Rules/Arrays/InArrayRuleTest.php
+++ tests/Feature/Rules/Arrays/InArrayRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Arrays;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class InArrayRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('BooleanRules')]
+class InArrayRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([1, [3, 5]])]
+    #[TestWith(['text', [3, 'text2']])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value, array $other): void
     {
-        $this->markTestSkipped();
+        $this->testException('in_array:other.*', ['field' => $value, 'other' => $other], 'in_array');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([1, [3, 5, 1]])]
+    #[TestWith(['text', [3, 'text2', 'text']])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value, array $other): void
+    {
+        $this->testSuccess('in_array:other.*', ['field' => $value, 'other' => $other]);
     }
 }
