6b1999d316f5b0f0b6cee9e1d68968a41cebc6b8
Denis YepBro <denis@yepbro.ru>
2025-02-01 15:51:08 +0300

    Array rule feature tests

--- tests/Feature/Rules/Arrays/ArrayRuleTest.php
+++ tests/Feature/Rules/Arrays/ArrayRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Arrays;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class ArrayRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('ArrayRules')]
+class ArrayRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([null])]
+    #[TestWith([true])]
+    #[TestWith(['string'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value): void
     {
-        $this->markTestSkipped();
+        $this->testException('array', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([[1, 'string']])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value): void
+    {
+        $this->testSuccess('array', $value);
     }
 }
