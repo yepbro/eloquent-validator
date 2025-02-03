6d28d6395b277c2adffcf73ea92d66a4db7fd228
Denis YepBro <denis@yepbro.ru>
2025-02-03 10:03:32 +0300

    Different string rule feature tests

---
+++ tests/Feature/Rules/Strings/DifferentRuleTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Feature\Rules\Strings;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
+use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
+
+#[Group('Rules')]
+#[Group('StringRules')]
+class DifferentRuleTest extends RuleTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    public function test_validation_of_incorrect_data_with_a_stringable_rule()
+    {
+        $this->testException('string|different:other', [
+            'other' => "string",
+            'field' => "string",
+        ], 'different');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['5', 5])]
+    #[TestWith(['one', 'two'])]
+    #[TestWith(['x', null])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value, mixed $other)
+    {
+        $this->testSuccess('string|different:other', [
+            'other' => $other,
+            'field' => $value,
+        ]);
+    }
+}
