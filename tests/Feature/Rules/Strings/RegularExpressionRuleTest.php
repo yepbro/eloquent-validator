30419ed32291ade5f97e19528dd878cd13f955ed
Denis YepBro <denis@yepbro.ru>
2025-02-03 20:21:52 +0300

    Regular Expression string rule feature tests

--- tests/Feature/Rules/Strings/RegularExpressionRuleTest.php
+++ tests/Feature/Rules/Strings/RegularExpressionRuleTest.php
@@ -3,14 +3,33 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class RegularExpressionRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class RegularExpressionRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['email'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('regex:/^.+@.+$/i', $value, 'regex');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['email@domain.com'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('regex:/^.+@.+$/i', $value);
     }
 }
