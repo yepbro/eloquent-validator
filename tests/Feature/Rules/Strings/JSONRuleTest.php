f17e9125bf831e6d536045d777ca817fb7529410
Denis YepBro <denis@yepbro.ru>
2025-02-03 19:55:07 +0300

    Json string rule feature tests

--- tests/Feature/Rules/Strings/JSONRuleTest.php
+++ tests/Feature/Rules/Strings/JSONRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class JSONRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class JSONRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['{"foo":"This is a \"quote\" and \invalid escape"}'])]
+    #[TestWith(['{"foo":30.5.6}'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('json', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['{"foo":"bar"}'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('json', $value);
     }
 }
