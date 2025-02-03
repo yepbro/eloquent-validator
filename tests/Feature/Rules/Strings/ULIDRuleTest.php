22b750491173409576a1bc5a1010021e4f84a21b
Denis YepBro <denis@yepbro.ru>
2025-02-03 20:01:42 +0300

    ULID string rule feature tests

--- tests/Feature/Rules/Strings/ULIDRuleTest.php
+++ tests/Feature/Rules/Strings/ULIDRuleTest.php
@@ -3,14 +3,33 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class ULIDRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class ULIDRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['01ARZ3NDEKTSV4RRFFQ69G5FAV1'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('ulid', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['01ARZ3NDEKTSV4RRFFQ69G5FAV'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('ulid', $value);
     }
 }
