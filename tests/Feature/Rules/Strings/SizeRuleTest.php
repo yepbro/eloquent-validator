09e774a310f484a99221ade591331bd84218dada
Denis YepBro <denis@yepbro.ru>
2025-02-03 19:25:24 +0300

    Size string rule feature tests

--- tests/Feature/Rules/Strings/SizeRuleTest.php
+++ tests/Feature/Rules/Strings/SizeRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class SizeRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class SizeRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([1234])]
+    #[TestWith(['six'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('size:4', $value, 'size.string');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['four'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('size:4', $value);
     }
 }
