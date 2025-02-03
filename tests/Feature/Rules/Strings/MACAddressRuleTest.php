48e9408169f98c488b9fba9544267f545ba9f758
Denis YepBro <denis@yepbro.ru>
2025-02-03 18:59:19 +0300

    MAC Address string rule feature tests

--- tests/Feature/Rules/Strings/MACAddressRuleTest.php
+++ tests/Feature/Rules/Strings/MACAddressRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class MACAddressRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class MACAddressRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['00:0A:95:9D:67:1M'])]
+    #[TestWith(['00FFFF'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('mac_address', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['00:0A:95:9D:67:16'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('mac_address', $value);
     }
 }
