5cb1bcfc48eb689cff179d29faeec844d80ed7f5
Denis YepBro <denis@yepbro.ru>
2025-02-03 19:01:42 +0300

    IP Address string rule feature tests

--- tests/Feature/Rules/Strings/IPAddressRuleTest.php
+++ tests/Feature/Rules/Strings/IPAddressRuleTest.php
@@ -3,14 +3,35 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class IPAddressRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class IPAddressRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['test.ru'])]
+    #[TestWith(['127.0.0.500'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('ip', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['127.0.0.1'])]
+    #[TestWith(['fe00:0000:0000:0001:0000:0000:0000:0092'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('ip', $value);
     }
 }
