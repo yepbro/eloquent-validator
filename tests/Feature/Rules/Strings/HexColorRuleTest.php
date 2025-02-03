9d4f2e99bdecf31644bc2dd8a82be8b1df3154bc
Denis YepBro <denis@yepbro.ru>
2025-02-03 18:57:30 +0300

    Hex Color string rule feature tests

--- tests/Feature/Rules/Strings/HexColorRuleTest.php
+++ tests/Feature/Rules/Strings/HexColorRuleTest.php
@@ -3,14 +3,37 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class HexColorRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class HexColorRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['grey'])]
+    #[TestWith(['00FFFF'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('hex_color', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['#000'])]
+    #[TestWith(['#FF00'])]
+    #[TestWith(['#FFBBCC'])]
+    #[TestWith(['#FFBBCC01'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('hex_color', $value);
     }
 }
