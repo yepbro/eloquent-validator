a6ba07af4d97ee6602987e7ba96908aec844924d
Denis YepBro <denis@yepbro.ru>
2025-02-03 10:08:56 +0300

    Doesnt End With string rule feature tests

--- tests/Feature/Rules/Strings/DoesntEndWithRuleTest.php
+++ tests/Feature/Rules/Strings/DoesntEndWithRuleTest.php
@@ -3,14 +3,35 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class DoesntEndWithRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class DoesntEndWithRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['long foo'])]
+    #[TestWith(['cafe bar'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('doesnt_end_with:foo,bar', $value, 'doesnt_end_with');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['start foo end'])]
+    #[TestWith(['no bar end'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('doesnt_end_with:foo,bar', $value);
     }
 }
