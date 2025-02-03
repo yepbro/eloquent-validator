c858727e89cbfc43b68f1c2c268aa45034e58766
Denis YepBro <denis@yepbro.ru>
2025-02-03 19:22:47 +0300

    Uppercase string rule feature tests

--- tests/Feature/Rules/Strings/UppercaseRuleTest.php
+++ tests/Feature/Rules/Strings/UppercaseRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class UppercaseRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class UppercaseRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['Go to'])]
+    #[TestWith(['world'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('uppercase', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['OK'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('uppercase', $value);
     }
 }
