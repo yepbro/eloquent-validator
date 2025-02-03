e1fe8e7195351f85f9ff15b61c50ccd65b1a11e6
Denis YepBro <denis@yepbro.ru>
2025-02-03 19:31:15 +0300

    Same string rule feature tests

--- tests/Feature/Rules/Strings/SameRuleTest.php
+++ tests/Feature/Rules/Strings/SameRuleTest.php
@@ -3,14 +3,40 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class SameRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class SameRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['two'])]
+    #[TestWith([1234])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('same:other', [
+            'field' => $value,
+            'other' => '1234',
+        ], 'same');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['four'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('same:other', [
+            'field' => $value,
+            'other' => $value,
+        ]);
     }
 }
