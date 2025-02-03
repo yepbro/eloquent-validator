0eda4097ebf2370d258e4145c28eb10a8bf81f6d
Denis YepBro <denis@yepbro.ru>
2025-02-03 19:21:34 +0300

    Lowercase string rule feature tests

--- tests/Feature/Rules/Strings/LowercaseRuleTest.php
+++ tests/Feature/Rules/Strings/LowercaseRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class LowercaseRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class LowercaseRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['Go to'])]
+    #[TestWith(['WORLD'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('lowercase', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['ok'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('lowercase', $value);
     }
 }
