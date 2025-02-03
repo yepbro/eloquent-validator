f9c6d2e24e1e543ba15b2abe623b1f26a9cc23b7
Denis YepBro <denis@yepbro.ru>
2025-02-03 20:04:55 +0300

    UUID string rule feature tests

--- tests/Feature/Rules/Strings/UUIDRuleTest.php
+++ tests/Feature/Rules/Strings/UUIDRuleTest.php
@@ -3,14 +3,39 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class UUIDRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class UUIDRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['01ARZ3NDEKTSV4RRFFQ69G5FAV'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('uuid', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['1e4f8b9c-5a6d-11ee-b962-0242ac120002'])]
+    #[TestWith(['3f8b9c5a-6d1e-3ee5-b962-0242ac120002'])]
+    #[TestWith(['5a6d1e4f-8b9c-4ee5-b962-0242ac120002'])]
+    #[TestWith(['6d1e4f8b-9c5a-5ee5-b962-0242ac120002'])]
+    #[TestWith(['1e4f8b9c-5a6d-61ee-b962-0242ac120002'])]
+    #[TestWith(['018b9c5a-6d1e-7ee5-b962-0242ac120002'])]
+    #[TestWith(['8b9c5a6d-1e4f-8ee5-b962-0242ac120002'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('uuid', $value);
     }
 }
