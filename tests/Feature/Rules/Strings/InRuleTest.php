0e072d79a8daabb87c03126f022ace5333f5a356
Denis YepBro <denis@yepbro.ru>
2025-02-03 19:49:20 +0300

    In string rule feature tests

--- tests/Feature/Rules/Strings/InRuleTest.php
+++ tests/Feature/Rules/Strings/InRuleTest.php
@@ -3,14 +3,33 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class InRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class InRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['toilet'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('in:foo,bar', $value, 'in');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['foo'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('in:foo,bar', $value);
     }
 }
