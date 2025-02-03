e1c494184308e0301011592a2ee90f3e0515f753
Denis YepBro <denis@yepbro.ru>
2025-02-03 10:07:58 +0300

    Doesnt Start With string rule feature tests

--- tests/Feature/Rules/Strings/DoesntStartWithRuleTest.php
+++ tests/Feature/Rules/Strings/DoesntStartWithRuleTest.php
@@ -3,14 +3,35 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class DoesntStartWithRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class DoesntStartWithRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['foo long'])]
+    #[TestWith(['bar cafe'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('doesnt_start_with:foo,bar', $value, 'doesnt_start_with');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['start foo'])]
+    #[TestWith(['no bar'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('doesnt_start_with:foo,bar', $value);
     }
 }
