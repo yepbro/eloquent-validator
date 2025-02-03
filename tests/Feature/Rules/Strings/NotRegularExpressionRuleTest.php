6ed7ace466308c8281c9eff376be052ae557ee8a
Denis YepBro <denis@yepbro.ru>
2025-02-03 20:22:56 +0300

    Not Regular Expression string rule feature tests

--- tests/Feature/Rules/Strings/NotRegularExpressionRuleTest.php
+++ tests/Feature/Rules/Strings/NotRegularExpressionRuleTest.php
@@ -3,14 +3,33 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class NotRegularExpressionRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class NotRegularExpressionRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['email@domain.com'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('not_regex:/^.+@.+$/i', $value, 'not_regex');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['email'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('not_regex:/^.+@.+$/i', $value);
     }
 }
