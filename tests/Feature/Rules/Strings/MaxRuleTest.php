806bd34ce75c3219000725d80f32c2d33f854f2b
Denis YepBro <denis@yepbro.ru>
2025-02-03 19:27:33 +0300

    Max string rule feature tests

--- tests/Feature/Rules/Strings/MaxRuleTest.php
+++ tests/Feature/Rules/Strings/MaxRuleTest.php
@@ -3,14 +3,34 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Strings;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class MaxRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('StringRules')]
+class MaxRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['eleven'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('max:4', $value, 'max.string');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['four'])]
+    #[TestWith(['two'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('max:4', $value);
     }
 }
