dd098b692866a2c0b49e3d33f80003f48230c427
Denis YepBro <denis@yepbro.ru>
2025-02-04 20:18:25 +0300

    Required Without utility rule feature tests

--- tests/Feature/Rules/Utilities/RequiredWithoutRuleTest.php
+++ tests/Feature/Rules/Utilities/RequiredWithoutRuleTest.php
@@ -3,14 +3,39 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Utilities;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class RequiredWithoutRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('UtilityRules')]
+class RequiredWithoutRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['ten'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('required_without:other', [
+            'field' => null,
+            'other' => null,
+        ], 'required_without');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['ten'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('required_without:other', [
+            'field' => $value,
+            'other' => null,
+        ]);
     }
 }
