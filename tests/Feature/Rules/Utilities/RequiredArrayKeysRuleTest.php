d0f8129984b64520c32ce41c026b87b2eaf8e86d
Denis YepBro <denis@yepbro.ru>
2025-02-04 20:30:30 +0300

    Required Array Keys utility rule feature tests

--- tests/Feature/Rules/Utilities/RequiredArrayKeysRuleTest.php
+++ tests/Feature/Rules/Utilities/RequiredArrayKeysRuleTest.php
@@ -3,14 +3,40 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Utilities;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class RequiredArrayKeysRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('UtilityRules')]
+class RequiredArrayKeysRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([null, 'no'])]
+    #[TestWith([['k' => 1, 'm' => 2], 'z'])]
+    #[TestWith([['k' => 1, 'm' => 2], 'k,z,m'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value, mixed $keys)
     {
-        $this->markTestSkipped();
+        $this->testException("required_array_keys:$keys", [
+            'field' => $value,
+        ], 'required_array_keys');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([['k' => 1, 'm' => 2], 'k'])]
+    #[TestWith([['k' => 1, 'm' => 2], 'm,k'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value, mixed $keys)
+    {
+        $this->testSuccess("required_array_keys:$keys", [
+            'field' => $value,
+        ]);
     }
 }
