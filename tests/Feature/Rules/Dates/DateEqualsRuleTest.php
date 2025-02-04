65c6f497be0ffde3fe95f176bc1121acf228847b
Denis YepBro <denis@yepbro.ru>
2025-02-04 21:02:18 +0300

    Date Equals rule feature tests

--- tests/Feature/Rules/Dates/DateEqualsRuleTest.php
+++ tests/Feature/Rules/Dates/DateEqualsRuleTest.php
@@ -3,14 +3,35 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Dates;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class DateEqualsRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('DateRules')]
+class DateEqualsRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['2024-12-22'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('date_equals:2024-12-12', $value, 'date_equals');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['10 September 2000'])]
+    #[TestWith(['2000-09-10'])]
+    #[TestWith(['10.09.2000'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('date_equals:2000-09-10', $value);
     }
 }
