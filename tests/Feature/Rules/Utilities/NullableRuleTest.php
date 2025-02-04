a794734e48ef559fcc31081ac1fa098c56b1c60b
Denis YepBro <denis@yepbro.ru>
2025-02-04 18:48:35 +0300

    Nullable utility rule feature tests

--- tests/Feature/Rules/Utilities/NullableRuleTest.php
+++ tests/Feature/Rules/Utilities/NullableRuleTest.php
@@ -3,14 +3,25 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Utilities;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class NullableRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('UtilityRules')]
+class NullableRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([null])]
+    #[TestWith(['null'])]
+    public function test_success(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testSuccess('nullable|string', $value);
     }
 }
