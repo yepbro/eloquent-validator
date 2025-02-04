af24479cb84534631f1e57a9e0a234586a4bfd95
Denis YepBro <denis@yepbro.ru>
2025-02-04 20:59:36 +0300

    Date rule feature tests

--- tests/Feature/Rules/Dates/DateRuleTest.php
+++ tests/Feature/Rules/Dates/DateRuleTest.php
@@ -3,14 +3,35 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Dates;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class DateRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('DateRules')]
+class DateRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['string'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('date', $value);
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['10 September 2000'])]
+    #[TestWith(['2023-05-30'])]
+    #[TestWith(['20.12.2024'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('date', $value);
     }
 }
