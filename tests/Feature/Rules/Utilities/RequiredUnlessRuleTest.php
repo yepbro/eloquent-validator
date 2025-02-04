a15b15f63fc79e5539da2f4cb10fe20d1cb39eba
Denis YepBro <denis@yepbro.ru>
2025-02-04 20:04:19 +0300

    Required Unless utility rule feature tests

--- tests/Feature/Rules/Utilities/RequiredUnlessRuleTest.php
+++ tests/Feature/Rules/Utilities/RequiredUnlessRuleTest.php
@@ -3,14 +3,43 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Utilities;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class RequiredUnlessRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('UtilityRules')]
+class RequiredUnlessRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([null, 'bar'])]
+    #[TestWith([[], 'bar'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value, mixed $other)
     {
-        $this->markTestSkipped();
+        $this->testException('required_unless:other,foo', [
+            'field' => $value,
+            'other' => $other,
+        ], 'required_unless');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['null', 'foo'])]
+    #[TestWith([[0], 'foo'])]
+    #[TestWith([null, 'foo'])]
+    #[TestWith(['s', 'bar'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value, mixed $other)
+    {
+        $this->testSuccess('required_unless:other,foo', [
+            'field' => $value,
+            'other' => $other,
+        ]);
     }
 }
