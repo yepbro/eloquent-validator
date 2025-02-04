86a813e01da2018683931c44e6af436d5f263fe5
Denis YepBro <denis@yepbro.ru>
2025-02-04 20:16:01 +0300

    Required With All utility rule feature tests

--- tests/Feature/Rules/Utilities/RequiredWithAllRuleTest.php
+++ tests/Feature/Rules/Utilities/RequiredWithAllRuleTest.php
@@ -3,14 +3,41 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Utilities;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class RequiredWithAllRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('UtilityRules')]
+class RequiredWithAllRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([null])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('required_with:other,another', [
+            'field' => $value,
+            'other' => 'seven',
+            'another' => 'seven',
+        ], 'required_with');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['ten'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('required_with:other,another', [
+            'field' => $value,
+            'other' => 'five',
+            'another' => 'seven',
+        ]);
     }
 }
