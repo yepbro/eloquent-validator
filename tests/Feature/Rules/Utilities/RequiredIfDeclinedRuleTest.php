c300e1aeea6f58e5c02217253f62c09f0a6c9cf1
Denis YepBro <denis@yepbro.ru>
2025-02-04 20:26:34 +0300

    Required If Declined utility rule feature tests

--- tests/Feature/Rules/Utilities/RequiredIfDeclinedRuleTest.php
+++ tests/Feature/Rules/Utilities/RequiredIfDeclinedRuleTest.php
@@ -3,14 +3,42 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Utilities;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class RequiredIfDeclinedRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('UtilityRules')]
+class RequiredIfDeclinedRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([null, 'no'])]
+    #[TestWith(['', '0'])]
+    #[TestWith([[], 'off'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value, mixed $other)
     {
-        $this->markTestSkipped();
+        $this->testException('required_if_declined:other', [
+            'field' => $value,
+            'other' => $other,
+        ], 'required_if_declined');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['ten', 'off'])]
+    #[TestWith([false, 'no'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value, mixed $other)
+    {
+        $this->testSuccess('required_if_declined:other', [
+            'field' => $value,
+            'other' => $other,
+        ]);
     }
 }
