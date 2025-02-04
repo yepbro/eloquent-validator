0788a889be29ca15234921952e2ba4ef5e0e92ab
Denis YepBro <denis@yepbro.ru>
2025-02-04 21:05:11 +0300

    Before Date rule feature tests

--- tests/Feature/Rules/Dates/BeforeRuleTest.php
+++ tests/Feature/Rules/Dates/BeforeRuleTest.php
@@ -3,14 +3,36 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Dates;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class BeforeRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('DateRules')]
+class BeforeRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['2024-12-22'])]
+    #[TestWith(['2024-12-12'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value)
     {
-        $this->markTestSkipped();
+        $this->testException('before:2024-12-12', $value, 'before');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith(['10 September 1999'])]
+    #[TestWith(['2000-09-09'])]
+    #[TestWith(['01.09.2000'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value)
+    {
+        $this->testSuccess('before:2000-09-10', $value);
     }
 }
