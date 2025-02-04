75178dea9c753a637f092630ae9e32c229226540
Denis YepBro <denis@yepbro.ru>
2025-02-04 20:53:28 +0300

    Added Between file rule feature tests

---
+++ tests/Feature/Rules/Files/BetweenRuleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace YepBro\EloquentValidator\Tests\Feature\Rules\Files;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+
+#[Group('Rules')]
+#[Group('FileRules')]
+class BetweenRuleTest extends FeatureTestCase
+{
+    use DatabaseMigrations;
+
+    public function test_skip()
+    {
+        $this->markTestIncomplete('The validation rule is not applicable to models.');
+    }
+}
