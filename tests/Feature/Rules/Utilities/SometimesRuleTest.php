5df51daf920ab145ee6c12019e95b1022f4d31d5
Denis YepBro <denis@yepbro.ru>
2025-02-04 19:15:13 +0300

    Skip sometimes utility rules feature tests

---
+++ tests/Feature/Rules/Utilities/SometimesRuleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace YepBro\EloquentValidator\Tests\Feature\Rules\Utilities;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
+
+#[Group('Rules')]
+#[Group('UtilityRules')]
+class SometimesRuleTest extends RuleTestCase
+{
+    use DatabaseMigrations;
+
+    public function test_skip()
+    {
+        $this->markTestIncomplete('The validation rule is not applicable to models.');
+    }
+}
