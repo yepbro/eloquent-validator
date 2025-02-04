55b12f1d2e43bd51acdf54d8963aed257bee4c19
Denis YepBro <denis@yepbro.ru>
2025-02-04 19:20:33 +0300

    Required If utility rule feature tests

--- tests/Feature/Rules/Utilities/RequiredIfRuleTest.php
+++ tests/Feature/Rules/Utilities/RequiredIfRuleTest.php
@@ -3,14 +3,42 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Utilities;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class RequiredIfRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('UtilityRules')]
+class RequiredIfRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([null, 'foo'])]
+    #[TestWith(['', 'foo'])]
+    #[TestWith([[], 'foo'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value, mixed $other)
     {
-        $this->markTestSkipped();
+        $this->testException('required_if:other,foo', [
+            'field' => $value,
+            'other' => $other,
+        ], 'required_if');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([0, 'foo'])]
+    #[TestWith([null, 'foo_bar'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value, mixed $other)
+    {
+        $this->testSuccess('required_if:other,foo', [
+            'field' => $value,
+            'other' => $other,
+        ]);
     }
 }
