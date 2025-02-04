23e3aaf7749638bd155b5402a88dc7604fd6be80
Denis YepBro <denis@yepbro.ru>
2025-02-04 19:05:17 +0300

    Prohibited Unless utility rule feature tests

--- tests/Feature/Rules/Utilities/ProhibitedUnlessRuleTest.php
+++ tests/Feature/Rules/Utilities/ProhibitedUnlessRuleTest.php
@@ -3,14 +3,43 @@
 namespace YepBro\EloquentValidator\Tests\Feature\Rules\Utilities;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestWith;
+use YepBro\EloquentValidator\Exceptions\ModelValidatorNotFound;
 use YepBro\EloquentValidator\Tests\Feature\FeatureTestCase;
+use YepBro\EloquentValidator\Tests\Feature\Rules\RuleTestCase;
 
-class ProhibitedUnlessRuleTest extends FeatureTestCase
+#[Group('Rules')]
+#[Group('UtilityRules')]
+class ProhibitedUnlessRuleTest extends RuleTestCase
 {
     use DatabaseMigrations;
 
-    public function test_ok()
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([false, 'foo_bar'])]
+    #[TestWith([0, 'foo_bar'])]
+    public function test_validation_of_incorrect_data_with_a_stringable_rule(mixed $value, mixed $other)
     {
-        $this->markTestSkipped();
+        $this->testException('prohibited_unless:other,foo', [
+            'field' => $value,
+            'other' => $other,
+        ], 'prohibited_unless');
+    }
+
+    /**
+     * @throws ModelValidatorNotFound
+     */
+    #[TestWith([null, 'foo_bar'])]
+    #[TestWith(['', 'foo_bar'])]
+    #[TestWith([[], 'foo_bar'])]
+    #[TestWith(['go', 'foo'])]
+    public function test_validation_of_correct_data_with_a_stringable_rule(mixed $value, mixed $other)
+    {
+        $this->testSuccess('prohibited_unless:other,foo', [
+            'field' => $value,
+            'other' => $other,
+        ]);
     }
 }
